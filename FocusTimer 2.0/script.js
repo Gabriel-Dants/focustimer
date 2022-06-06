4f6b29d173b9fadacba1640f80c3e4e1fdd86db1
Gabriel Dantas <66450718+Gabriel-Dants@users.noreply.github.com>
2022-06-06 10:49:40 -0300

    Add files via upload

--- FocusTimer 2.0/script.js
+++ FocusTimer 2.0/script.js
@@ -17,6 +17,9 @@ let cardCafeteria = document.querySelector('.cafeteria')
 let cardCafeteria2 = document.querySelector('.cafeteria2')
 let cardLareira = document.querySelector('.lareira')
 let cardLareira2 = document.querySelector('.lareira2')
+let buttonLight = document.querySelector('.sun')
+let buttonDark = document.querySelector('.moon')
+
 
 
 //Variáveis de Efeitos Sonoros
@@ -28,6 +31,36 @@ const chuvaSound = new Audio("sounds/Chuva.wav")
 const cafeteriaSound = new Audio("sounds/Cafeteria.wav")
 const lareiraSound = new Audio("sounds/Lareira.wav")
 
+let sliderFloresta = document.querySelector('.floresta2 > input[type="range"]')
+let sliderChuva = document.querySelector('.chuva2 > input[type="range"]')
+let sliderCafeteria = document.querySelector('.cafeteria2 > input[type="range"]')
+let sliderLareira = document.querySelector('.lareira2 > input[type="range"]')
+
+
+//variáveis para o dark theme
+const backgroundContainer = document.querySelector('.container')
+
+let cardFlorestaDark = document.querySelector('.floresta_dark01')
+let cardFlorestaDark2 = document.querySelector('.floresta_dark02')
+let cardChuvaDark = document.querySelector('.chuva_dark01')
+let cardChuvaDark2 = document.querySelector('.chuva_dark02')
+let cardCafeteriaDark = document.querySelector('.cafeteria_dark01')
+let cardCafeteriaDark2 = document.querySelector('.cafeteria_dark02')
+let cardLareiraDark = document.querySelector('.lareira_dark01')
+let cardLareiraDark2 = document.querySelector('.lareira_dark02')
+
+let buttonPlayDark = document.querySelector('.play_dark01')
+let buttonPauseDark = document.querySelector('.pause_dark02')
+let buttonStopDark = document.querySelector('.stop_dark02')
+let buttonCountDark = document.querySelector('.count_dark01')
+
+let buttonPlusDark = document.querySelector('.add5_dark01')
+let buttonMinusDark = document.querySelector('.sub5_dark01')
+
+let sliderFlorestaDark = document.querySelector('.floresta_dark02 > input[type="range"]')
+let sliderChuvaDark = document.querySelector('.chuva_dark02 > input[type="range"]')
+let sliderCafeteriaDark = document.querySelector('.cafeteria_dark02 > input[type="range"]')
+let sliderLareiraDark = document.querySelector('.lareira_dark02 > input[type="range"]')
 
 //Outras variáveis
 let minutesDisplay = document.querySelector('.minutes')
@@ -38,6 +71,7 @@ let seconds = Number(secondsDisplay.textContent)
 
 let newMinutes
 let pauseCountDown
+
 
 
 //Atualização do Timer
@@ -161,6 +195,8 @@ cardLareira2.addEventListener('click', function (){
     lareiraSound.pause()
 })
 
+// Eventos de Clique nos botões de aumentar ou diminuir o contador
+
 buttonPlus.addEventListener('click', function (){
     updateTimerDisplay(++minutes,0)
     buttonPressAudio.play()
@@ -170,6 +206,40 @@ buttonMinus.addEventListener('click', function (){
     updateTimerDisplay(--minutes,0)
     buttonPressAudio.play()
 })
+
+// Eventos de Clique nos botões de Light e Dark Theme
+
+buttonLight.addEventListener('click', function (){
+    buttonLight.classList.add('hide')
+    buttonDark.classList.remove('hide')
+    AddDarkMode()
+    buttonPressAudio.play()
+})
+
+buttonDark.addEventListener('click', function (){
+    buttonDark.classList.add('hide')
+    buttonLight.classList.remove('hide')
+    buttonPressAudio.play()
+})
+
+// Eventos de Clique nos Sliders
+
+sliderFloresta.addEventListener("input", function(){
+    florestaSound.volume = sliderFloresta.value
+});
+
+sliderChuva.addEventListener("input", function(){
+    chuvaSound.volume = sliderChuva.value
+});
+
+sliderCafeteria.addEventListener("input", function(){
+    cafeteriaSound.volume = sliderCafeteria.value
+});
+
+sliderLareira.addEventListener("input", function(){
+    lareiraSound.volume = sliderLareira.value
+});
+
 
 
 // Função de Timer
@@ -196,19 +266,146 @@ function countdown () {
 }
 
 
-/* 
-O que acontece ao clicar no botão de DarkMode:
-    1. Tela de Fundo fica preta
-    2. Texto fica Branco
-    3. Ativa outros botões do controle, onde eles são brancos
-    4. Ativa outros cards, onde eles são brancos com fundo meio cinza
-    5. Em pararelo, quando clicar nos botões e card. Ativará a versão azul que já está implementada
+function AddDarkMode () {
+    backgroundContainer.classList.add('dark')
+    cardFloresta.classList.add('hide')
+    cardFloresta2.classList.add('hide')
+    cardChuva.classList.add('hide')
+    cardChuva2.classList.add('hide')
+    cardCafeteria.classList.add('hide')
+    cardCafeteria2.classList.add('hide')
+    cardLareira.classList.add('hide')
+    cardLareira2.classList.add('hide')
+    buttonPlay.classList.add('hide')
+    buttonPause.classList.add('hide')
+    buttonCount.classList.add('hide')
+    buttonStop.classList.add('hide')
+    buttonPlus.classList.add('hide')
+    buttonMinus.classList.add('hide')
 
-*/
 
-/* 
-Sobre a mudança de Volume:
-    1. Preciso vê como fazer para implementar mudança de volume nos arquivos de áudio
-    2. Implementar o Slider em cada um dos cards (Provavelmente teremos uma div com imagem e slider e depois fazer um posicionamento relativo/absoluto para colocar por cima)
-    3. Referência cada slider com classes para conseguirmos implementar no JS a mudança de volume de cada som para cada slider específico
-*/
+    cardFlorestaDark.classList.remove('hide')
+    cardChuvaDark.classList.remove('hide')
+    cardCafeteriaDark.classList.remove('hide')
+    cardLareiraDark.classList.remove('hide')
+    buttonPlayDark.classList.remove('hide')
+    buttonCountDark.classList.remove('hide')
+    buttonPlusDark.classList.remove('hide')
+    buttonMinusDark.classList.remove('hide')
+
+    florestaSound.pause()
+    chuvaSound.pause()
+    cafeteriaSound.pause()
+    lareiraSound.pause()
+    resetTimer()
+
+    
+
+}
+
+cardFlorestaDark.addEventListener('click', function() {
+    cardFlorestaDark.classList.add('hide')
+    cardFlorestaDark2.classList.remove('hide')
+    buttonPressAudio.play()
+    florestaSound.loop = true
+    florestaSound.play()
+})
+
+cardFlorestaDark2.addEventListener('click', function (){
+    cardFlorestaDark2.classList.add('hide')
+    cardFlorestaDark.classList.remove('hide')
+    buttonPressAudio.play()
+    florestaSound.pause()
+})
+
+
+cardChuvaDark.addEventListener('click', function (){
+    cardChuvaDark.classList.add('hide')
+    cardChuvaDark2.classList.remove('hide')
+    buttonPressAudio.play()
+    chuvaSound.loop = true
+    chuvaSound.play()
+})
+
+cardChuvaDark2.addEventListener('click', function (){
+    cardChuvaDark2.classList.add('hide')
+    cardChuvaDark.classList.remove('hide')
+    buttonPressAudio.play()
+    chuvaSound.pause()
+})
+
+cardCafeteriaDark.addEventListener('click', function (){
+    cardCafeteriaDark.classList.add('hide')
+    cardCafeteriaDark2.classList.remove('hide')
+    buttonPressAudio.play()
+    cafeteriaSound.loop = true
+    cafeteriaSound.play()
+})
+
+cardCafeteriaDark2.addEventListener('click', function (){
+    cardCafeteriaDark2.classList.add('hide')
+    cardCafeteriaDark.classList.remove('hide')
+    buttonPressAudio.play()
+    cafeteriaSound.pause()
+})
+
+cardLareiraDark.addEventListener('click', function (){
+    cardLareiraDark.classList.add('hide')
+    cardLareiraDark2.classList.remove('hide')
+    buttonPressAudio.play()
+    lareiraSound.loop = true
+    lareiraSound.play()
+})
+
+cardLareiraDark2.addEventListener('click', function (){
+    cardLareiraDark2.classList.add('hide')
+    cardLareiraDark.classList.remove('hide')
+    buttonPressAudio.play()
+    lareiraSound.pause()
+})
+
+sliderFlorestaDark.addEventListener("input", function(){
+    florestaSound.volume = sliderFlorestaDark.value
+});
+
+sliderChuvaDark.addEventListener("input", function(){
+    chuvaSound.volume = sliderChuvaDark.value
+});
+
+sliderCafeteriaDark.addEventListener("input", function(){
+    cafeteriaSound.volume = sliderCafeteriaDark.value
+});
+
+sliderLareiraDark.addEventListener("input", function(){
+    lareiraSound.volume = sliderLareiraDark.value
+});
+
+buttonPlayDark.addEventListener('click', function (){
+    buttonPlayDark.classList.add('hide')
+    buttonPauseDark.classList.remove('hide')
+    buttonCountDark.classList.add('hide')
+    buttonStopDark.classList.remove('hide')
+    buttonPressAudio.play()
+})
+
+buttonPauseDark.addEventListener('click', function (){
+    buttonPlayDark.classList.remove('hide')
+    buttonPauseDark.classList.add('hide')
+    buttonPressAudio.play()
+})
+
+buttonCountDark.addEventListener('click', function (){
+    buttonCountDark.classList.add('hide')
+    buttonStopDark.classList.remove('hide')
+    buttonPressAudio.play()
+})
+
+buttonStopDark.addEventListener('click', function (){
+    buttonStopDark.classList.add('hide')
+    buttonCountDark.classList.remove('hide')
+    buttonPlayDark.classList.remove('hide')
+    buttonPauseDark.classList.add('hide')
+    buttonPressAudio.play()
+})
+
+// Próxima Tarefa é organizar o código e começar a montar a função de countdown na função dark. Depois de tudo isso aplicar o botão de Light theme
